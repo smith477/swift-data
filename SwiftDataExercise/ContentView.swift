f96e90decdcbbf81c3b8f570574130995492ab3e
Dusan <dusankovacevic47@gmail.com>
2023-08-08 12:31:15 +0200

    Initial Commit

---
+++ SwiftDataExercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataExercise
+//
+//  Created by Dusan Kovacevic on 8/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataExercise/SwiftDataExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExerciseApp.swift
+//  SwiftDataExercise
+//
+//  Created by Dusan Kovacevic on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
